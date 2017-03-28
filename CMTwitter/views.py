25fc489b70952735abb9d683e5770f57cefb53d9
Phoenix Chen <phoenix0722chen@gmail.com>
2017-03-28 04:46:03 -0700

    Add Bootstrap
    Add Bootstrap CSS, JS, FONT

--- CMTwitter/views.py
+++ CMTwitter/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, render_to_response
 
 def index(request):
+    print("1")
+    if request.session.get('logged_in', False):
+        print("2")
+        return redirect('/home')
     return render(request, 'index.html')
 
 def home(request):
---
+++ user/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render, render_to_response
+
+def login(request):
+    pass
+    
+
+def logout(request):
+    pass
+
+def signup(request):
+    pass
